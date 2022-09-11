26270531aff4c4c9e9abe3e05df97b150094d71d
jenilparmar1 <jenilparmar1@gmail.com>
2022-09-11 16:26:47 +0530

    financial year and services

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function dashBoard() {
     window.location.replace("/index/index.html");
 }
+
+function company() {
+    window.location.replace("/masters/company/comapny.html");
+}
+
+function financial() {
+  window.location.replace("/masters/financial/financial.html");
+}
+
+function service() {
+  window.location.replace("/masters/service/service.html");
+}
+
